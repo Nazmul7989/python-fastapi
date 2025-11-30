0014912d3f6ddf9945bf6e752b6d5ec5d64ea34c
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2025-11-30 20:11:08 +0600

    practicing fast api

---
+++ main.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+
+app = FastAPI()
+
+@app.get("/")
+def index():
+    return {"data": "Hello World"}
+
+@app.get("/about")
+def about():
+    return {"data": "About Page"}
+
+@app.get("/contact")
+def contact():
+    return {"data": "Contact Page"}
+
+@app.get("/users")
+def users(limit: int = 10, active: Optional[bool] = None):
+    #Write logic to fetch users from database
+    return {"data": f"Showing {limit} users", "active": active}
+
+@app.get("/users/{user_id}")
+def read_user(user_id: int):
+    #Write logic to fetch user from database
+    return {"data": user_id}
+
+class User(BaseModel):
+    name: str
+    age: int
+    active: bool
+    address: Optional[str] = None
+
+@app.post("/users")
+def create_user(user: User):
+    #Write logic to create user in database
+    return {"data": user}
+
+
+
+
+
+
+
